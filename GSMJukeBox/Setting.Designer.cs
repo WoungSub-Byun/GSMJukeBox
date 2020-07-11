d54abf8731d271a9239819812bc78210b5dbddbd
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-07-11 13:50:10 +0900

    [UPDATE] setting page

--- GSMJukeBox/Setting.Designer.cs
+++ GSMJukeBox/Setting.Designer.cs
@@ -31,18 +31,23 @@
             this.DatePicker = new MetroFramework.Controls.MetroDateTime();
             this.Button_SetTime = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.TimePicker = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TimePicker = new System.Windows.Forms.ComboBox();
             this.btnGoMainForm = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // DatePicker
             // 
             this.DatePicker.CalendarFont = new System.Drawing.Font("카카오 Regular", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.DatePicker.Location = new System.Drawing.Point(64, 69);
+            this.DatePicker.Location = new System.Drawing.Point(47, 74);
             this.DatePicker.MinimumSize = new System.Drawing.Size(0, 29);
             this.DatePicker.Name = "DatePicker";
             this.DatePicker.Size = new System.Drawing.Size(200, 39);
@@ -50,7 +55,7 @@
             // 
             // Button_SetTime
             // 
-            this.Button_SetTime.Location = new System.Drawing.Point(380, 212);
+            this.Button_SetTime.Location = new System.Drawing.Point(379, 227);
             this.Button_SetTime.Name = "Button_SetTime";
             this.Button_SetTime.Size = new System.Drawing.Size(75, 45);
             this.Button_SetTime.TabIndex = 2;
@@ -59,6 +64,11 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.comboBox1);
@@ -73,10 +83,96 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "예약시간 설정";
             // 
-            // TimePicker
+            // label3
             // 
-            this.TimePicker.FormattingEnabled = true;
-            this.TimePicker.Items.AddRange(new object[] {
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(446, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "분";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(341, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "시";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59"});
+            this.comboBox2.Location = new System.Drawing.Point(381, 161);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(59, 39);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
             "1",
             "2",
             "3",
@@ -101,10 +197,28 @@
             "22",
             "23",
             "24"});
-            this.TimePicker.Location = new System.Drawing.Point(64, 129);
-            this.TimePicker.Name = "TimePicker";
-            this.TimePicker.Size = new System.Drawing.Size(59, 39);
-            this.TimePicker.TabIndex = 3;
+            this.comboBox3.Location = new System.Drawing.Point(276, 161);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(59, 39);
+            this.comboBox3.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "분";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "시";
             // 
             // comboBox1
             // 
@@ -169,28 +283,43 @@
             "57",
             "58",
             "59"});
-            this.comboBox1.Location = new System.Drawing.Point(169, 129);
+            this.comboBox1.Location = new System.Drawing.Point(128, 161);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(59, 39);
             this.comboBox1.TabIndex = 4;
             // 
-            // label1
+            // TimePicker
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 132);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 31);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "시";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(234, 132);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 31);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "분";
+            this.TimePicker.FormattingEnabled = true;
+            this.TimePicker.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24"});
+            this.TimePicker.Location = new System.Drawing.Point(23, 161);
+            this.TimePicker.Name = "TimePicker";
+            this.TimePicker.Size = new System.Drawing.Size(59, 39);
+            this.TimePicker.TabIndex = 3;
             // 
             // btnGoMainForm
             // 
@@ -201,6 +330,15 @@
             this.btnGoMainForm.TabIndex = 7;
             this.btnGoMainForm.Text = "메인으로";
             this.btnGoMainForm.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "~";
             // 
             // Setting
             // 
@@ -227,5 +365,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button btnGoMainForm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label5;
     }
 }
--- GSMJukeBox/Setting.cs
+++ GSMJukeBox/Setting.cs
@@ -16,5 +16,6 @@ namespace GSMJukeBox
         {
             InitializeComponent();
         }
+
     }
 }
